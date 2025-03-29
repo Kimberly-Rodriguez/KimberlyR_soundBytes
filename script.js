13290d9853b3ae0bded3a5c2ddb244f0f67c9085
kimberly rodriguez <krodriguez.ucla@gmail.com>
2025-03-28 21:34:20 -0700

    adding slideshow effect and new instrument

--- script.js
+++ script.js
@@ -20,8 +20,40 @@ function removeTransition(event) {
   if (event.propertyName !== "transform") return; 
   this.classList.remove("playing");
 }
+//slide show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
 
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 document.addEventListener("keydown", playSound);
-keys.forEach((key) => key.addEventListener("click", (event) => playSound({ keyCode: event.target.getAttribute("data-key") }))); // Add click support
+keys.forEach((key) => key.addEventListener("click", (event) => playSound({ keyCode: event.target.getAttribute("data-key") }))); 
